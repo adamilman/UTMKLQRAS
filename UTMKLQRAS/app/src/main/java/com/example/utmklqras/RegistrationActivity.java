9f2241cf1c07c6cc324c89330ccfaab35e9cec60
Adam Ilman <adamilmansuhaimi@gmail.com>
2020-07-29 21:25:55 +0800

    register page function

---
+++ UTMKLQRAS/app/src/main/java/com/example/utmklqras/RegistrationActivity.java
@@ -0,0 +1,72 @@
+package com.example.utmklqras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText userName, userPassword, userEmail;
+    private Button regButton;
+    private TextView userLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        setupUIViews();
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(validate()){
+
+                    //upload data to database
+
+                }
+            }
+        });
+
+        userLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void setupUIViews(){
+
+        userName = findViewById(R.id.etUserName);
+        userPassword = findViewById(R.id.etUserPassword);
+        userEmail = findViewById(R.id.etUserEmail);
+        regButton = findViewById(R.id.btnRegister);
+        userLogin = findViewById(R.id.tvUserLogin);
+
+    }
+
+    private Boolean validate() {
+
+        Boolean result = false;
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+
+        if(name.isEmpty() && password.isEmpty() && email.isEmpty()) {
+
+            Toast.makeText(this, "Please enter all the detils", Toast.LENGTH_SHORT).show();
+        }else{
+             result = true;
+            }
+
+         return result;
+        }
+    }
+

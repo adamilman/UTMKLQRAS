e8d134091c3d4c1363a6b923fdb4e822f05c359d
Amir Hakim <mahakim2000@gmail.com>
2020-09-08 15:09:59 +0800

    ui and function modification

---
+++ UTMKLQRAS/app/src/main/java/com/example/utmklqras/UserDataLecturerActivity.java
@@ -0,0 +1,13 @@
+package com.example.utmklqras;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserDataLecturerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data_lecturer);
+    }
+}
